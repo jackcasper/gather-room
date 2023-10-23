033b8ec079bedf349c0964a9d2a78f69b730a8cc
jackcasper <74534594+jackcasper@users.noreply.github.com>
2023-10-23 13:15:46 +0200

    Update PlayerUpdateCommand.ts

--- server/rooms/commands/PlayerUpdateCommand.ts
+++ server/rooms/commands/PlayerUpdateCommand.ts
@@ -9,13 +9,19 @@ type Payload = {
   anim: string
 }
 
+// Command to update the state of a player
 export default class PlayerUpdateCommand extends Command<IOfficeState, Payload> {
   execute(data: Payload) {
+    // Extract client, x, y, and anim from the payload
     const { client, x, y, anim } = data
 
+    // Get the player object associated with the client's session ID from the room's state
     const player = this.room.state.players.get(client.sessionId)
 
+    // Check if the player exists (is not null or undefined)
     if (!player) return
+
+    // Update the player's x, y, and anim properties with the values from the payload
     player.x = x
     player.y = y
     player.anim = anim
