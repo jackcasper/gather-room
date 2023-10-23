59d605d32ffc1bba2f967d09da53c162c3fe0036
jackcasper <74534594+jackcasper@users.noreply.github.com>
2023-10-23 13:20:24 +0200

    Update PlayerUpdateNameCommand.ts

--- server/rooms/commands/PlayerUpdateNameCommand.ts
+++ server/rooms/commands/PlayerUpdateNameCommand.ts
@@ -7,13 +7,19 @@ type Payload = {
   name: string
 }
 
+// Command to update the name of a player
 export default class PlayerUpdateNameCommand extends Command<IOfficeState, Payload> {
   execute(data: Payload) {
+    // Extract client and name from the payload
     const { client, name } = data
 
+    // Get the player object associated with the client's session ID from the room's state
     const player = this.room.state.players.get(client.sessionId)
 
+    // Check if the player exists (is not null or undefined)
     if (!player) return
+
+    // Update the player's name with the provided name from the payload
     player.name = name
   }
 }
