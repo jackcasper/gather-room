7be7bc0cfde0dab7656b045fa830b52c1b15fa1e
jackcasper <74534594+jackcasper@users.noreply.github.com>
2023-10-31 12:21:47 +0100

    Update OfficeState.ts

--- server/rooms/schema/OfficeState.ts
+++ server/rooms/schema/OfficeState.ts
@@ -7,48 +7,57 @@ import {
   IChatMessage,
 } from '../../../types/IOfficeState'
 
+// Define a class for representing a player
 export class Player extends Schema implements IPlayer {
-  @type('string') name = ''
-  @type('number') x = 705
-  @type('number') y = 500
-  @type('string') anim = 'adam_idle_down'
-  @type('boolean') readyToConnect = false
-  @type('boolean') videoConnected = false
+  @type('string') name = ''              // Player's name
+  @type('number') x = 705                // Player's X-coordinate
+  @type('number') y = 500                // Player's Y-coordinate
+  @type('string') anim = 'adam_idle_down' // Player's animation state
+  @type('boolean') readyToConnect = false // Player's connection readiness
+  @type('boolean') videoConnected = false // Player's video connection status
 }
 
+// Define a class for representing a computer
 export class Computer extends Schema implements IComputer {
-  @type({ set: 'string' }) connectedUser = new SetSchema<string>()
+  @type({ set: 'string' }) connectedUser = new SetSchema<string>() // Users connected to the computer
 }
 
+// Define a class for representing a whiteboard
 export class Whiteboard extends Schema implements IWhiteboard {
-  @type('string') roomId = getRoomId()
-  @type({ set: 'string' }) connectedUser = new SetSchema<string>()
+  @type('string') roomId = getRoomId()        // Unique ID for the whiteboard room
+  @type({ set: 'string' }) connectedUser = new SetSchema<string>() // Users connected to the whiteboard
 }
 
+// Define a class for representing a chat message
 export class ChatMessage extends Schema implements IChatMessage {
-  @type('string') author = ''
-  @type('number') createdAt = new Date().getTime()
-  @type('string') content = ''
+  @type('string') author = ''                   // Message author's name
+  @type('number') createdAt = new Date().getTime() // Timestamp of message creation
+  @type('string') content = ''                   // Message content
 }
 
+// Define a class for representing the entire office state
 export class OfficeState extends Schema implements IOfficeState {
-  @type({ map: Player })
+  @type({ map: Player })     // Map of player objects
   players = new MapSchema<Player>()
 
-  @type({ map: Computer })
+  @type({ map: Computer })   // Map of computer objects
   computers = new MapSchema<Computer>()
 
-  @type({ map: Whiteboard })
+  @type({ map: Whiteboard }) // Map of whiteboard objects
   whiteboards = new MapSchema<Whiteboard>()
 
-  @type([ChatMessage])
+  @type([ChatMessage])      // Array of chat messages
   chatMessages = new ArraySchema<ChatMessage>()
 }
 
+// Set to keep track of whiteboard room IDs
 export const whiteboardRoomIds = new Set<string>()
+
+// Characters used to generate random room IDs
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 const charactersLength = characters.length
 
+// Function to generate a unique room ID for a whiteboard
 function getRoomId() {
   let result = ''
   for (let i = 0; i < 12; i++) {
@@ -59,6 +68,6 @@ function getRoomId() {
     return result
   } else {
     console.log('roomId exists, remaking another one.')
-    getRoomId()
+    getRoomId() // Recursively try to generate a new ID if it already exists
   }
 }
