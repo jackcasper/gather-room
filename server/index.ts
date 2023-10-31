2203b5af9cd581dfaad6708d3812c717a29b1e99
jackcasper <74534594+jackcasper@users.noreply.github.com>
2023-10-31 12:43:36 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -9,38 +9,51 @@ import { RoomType } from '../types/Rooms'
 
 import { HdmGather } from './rooms/HdmGather'
 
+// Define the port number to listen on
 const port = Number(process.env.PORT || 2569)
 const app = express()
 
+// Enable Cross-Origin Resource Sharing (CORS) to allow requests from different origins
 app.use(cors())
-app.use(express.json())
-// app.use(express.static('dist'))
 
+// Parse JSON in request bodies
+app.use(express.json())
+
+// Create an HTTP server using the Express app
 const server = http.createServer(app)
+
+// Create a Colyseus game server
 const gameServer = new Server({
   server,
 })
 
-// register room handlers
+// Register different room handlers
+
+// Register the LobbyRoom for the lobby
 gameServer.define(RoomType.LOBBY, LobbyRoom)
+
+// Register the HdmGather room as a public lobby
 gameServer.define(RoomType.PUBLIC, HdmGather, {
   name: 'Public Lobby',
   description: 'For making friends and familiarizing yourself with the controls',
   password: null,
   autoDispose: false,
 })
+
+// Register the HdmGather room for custom lobbies and enable real-time listing
 gameServer.define(RoomType.CUSTOM, HdmGather).enableRealtimeListing()
 
 /**
  * Register @colyseus/social routes
  *
- * - uncomment if you want to use default authentication (https://docs.colyseus.io/server/authentication/)
- * - also uncomment the import statement
+ * - Uncomment if you want to use default authentication (https://docs.colyseus.io/server/authentication/)
+ * - Also uncomment the import statement
  */
 // app.use("/", socialRoutes);
 
-// register colyseus monitor AFTER registering your room handlers
+// Register the Colyseus monitor for monitoring server activity
 app.use('/colyseus', monitor())
 
+// Start the game server and listen on the specified port
 gameServer.listen(port)
 console.log(`Listening on ws://localhost:${port}`)
